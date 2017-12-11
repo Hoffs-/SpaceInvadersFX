af8acede45351a1673641acf2eaa49fec7720f46
Ignas <ignasmaslinskas@gmail.com>
2017-12-11 17:27:49 +0200

    Made custom level builder more powerful by being able to specify multiple entities for levels.

--- src/main/java/com/ignasm/spaceinvaders/LevelBuilder.java
+++ src/main/java/com/ignasm/spaceinvaders/LevelBuilder.java
@@ -6,6 +6,8 @@ import com.ignasm.spaceinvaders.entities.EnemyTwo;
 import com.ignasm.spaceinvaders.entities.ShipEntity;
 import javafx.scene.layout.Pane;
 
+import java.util.Arrays;
+
 public class LevelBuilder {
     public static final double OFFSET_X = 50;
     public static final double OFFSET_Y = 12;
@@ -18,34 +20,23 @@ public class LevelBuilder {
     }
 
     public ShipEntity[][] generateStandardLevel() {
-        ShipEntity[][] enemyEntities = new ShipEntity[6][10];
-
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < enemyEntities[i].length; j++) {
-                enemyEntities[i][j] = new EnemyThree();
-            }
-        }
-
-        for (int i = 2; i < 4; i++) {
-            for (int j = 0; j < enemyEntities[i].length; j++) {
-                enemyEntities[i][j] = new EnemyTwo();
-            }
-        }
-
-        for (int i = 4; i < 6; i++) {
-            for (int j = 0; j < enemyEntities[i].length; j++) {
-                enemyEntities[i][j] = new EnemyOne();
-            }
-        }
-        placeEnemies(enemyEntities);
-        return enemyEntities;
+        return generateCustomLevel(6, 10, new EnemyThree(), new EnemyTwo(), new EnemyOne());
     }
 
-    public ShipEntity[][] generateCustomLevel(int rows, int columns, ShipEntity entity) {
+    public ShipEntity[][] generateCustomLevel(int rows, int columns, ShipEntity... entities) {
+        if (entities.length > rows) {
+            entities = Arrays.copyOfRange(entities, 0, rows);
+        }
+
+        int sliceSize = (int) Math.ceil((double) rows / (double) entities.length);
         ShipEntity[][] enemyEntities = new ShipEntity[rows][columns];
-        for (int i = 0; i < enemyEntities.length; i++) {
-            for (int j = 0; j < enemyEntities[i].length; j++) {
-                enemyEntities[i][j] = new ShipEntity(entity.getImage(), entity.getEntityWidth(), entity.getEntityHeight(), entity.getAnimationDuration());
+
+        for (int k = 0; k < entities.length; k++) {
+            for (int i = k * sliceSize; i < Math.min(enemyEntities.length, (k * sliceSize) + sliceSize); i++) {
+                for (int j = 0; j < enemyEntities[i].length; j++) {
+                    enemyEntities[i][j] = new ShipEntity(entities[k].getImage(), entities[k].getEntityWidth(), entities[k].getEntityHeight(), entities[k].getAnimationDuration());
+                    System.out.println(k + " [" + i + "][" + j + "]");
+                }
             }
         }
         placeEnemies(enemyEntities);
